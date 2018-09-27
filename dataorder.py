8f8da49b8d0f2242f14c3179ab5ec08504fa8e0e
Gary Harrison <GHarrison@clarkcountynv.gov>
2018-09-27 11:31:10 -0700

    Updated print statements and verison number

--- dataorder.py
+++ dataorder.py
@@ -2,12 +2,10 @@
 # data a customer is ordering (factoring in the data discount) and returns
 # the appropriate charge.
 
-print "\nGISMO Data Order Calculator v0.3"
-print "Last modified: 9-19-2013\n\n"
+print("\nGISMO Data Order Calculator v0.4")
+print("Last modified: 9-19-2013\n\n")
 
-numsections = int(raw_input("Enter number of sections requested: "))
-
-print " "
+numsections = int(raw_input("Enter number of sections requested: \n"))
 
 numfree = numsections / 4       # Calculate the number of free sections.
 numpay = numsections - numfree  # Calculate the number of paid sections.
@@ -16,19 +14,17 @@ aerialprice = 20 * numpay   # Calculate the charge for number of sections of aer
 parcelprice = 50 * numpay   # Calculate the charge for number of parcels.
 total = aerialprice + parcelprice   # Calculate the charge for both aerials and parcels.
 
-print "Number of free sections will be:    %d" % (numfree)
-print "Number of charged sections will be: %d" % (numpay)
-print " "
+print("Number of free sections will be:    %d") % (numfree)
+print("Number of charged sections will be: %d\n")% (numpay)
 
 if parcelprice >= 2500:
-  print "Charge for parcels is $2,500.00"
-  print "**Customer should order full parcel layer.**"
+  print("Charge for parcels is $2,500.00")
+  print("**Customer should order full parcel layer.**")
   
 else:
-  print "Charge for parcels is: $%.2f" % (parcelprice)
+  print("Charge for parcels is: $%.2f") % (parcelprice)
 
-print "Charge for aerials is: $%.2f" % (aerialprice)
-print " "
-print " "
+print("Charge for aerials is: $%.2f\n\n") % (aerialprice)
 
 raw_input("Press Enter to exit")
+
